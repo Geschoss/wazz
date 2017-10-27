f1fdd93c390790550045815c6dd7eb663a7af737
p.e.kolomnikov <p.e.kolomnikov@macbookpro-pekolomnikov.local>
2017-10-27 09:22:45 +0300

    ссылки

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
 (function(doc) {
   "use strict";
   const $ = querySelector(doc);
+  const findAll = querySelectorAll(doc);
 
   const menuBtn = $('.c-hamburger');
   const m_menu = $('.m-menu__container');
+  const menu_items = findAll('.menu__item');
   const m_menuClassList = m_menu.classList;
 
   menuBtn.addEventListener( 'click', function(e) {
@@ -12,6 +14,15 @@
       toggleClass(classList, 'is-active');
       toggleClass(m_menuClassList, 'opened');
   });
+  menu_items.forEach(item => {
+      item.addEventListener('click', closeMenu)
+  })
+
+
+  function closeMenu(e) {
+      menuBtn.classList.remove('is-active');
+      m_menuClassList.remove('opened');
+  }
 
   function toggleClass(classList, className) {
       (classList.contains(className) === true) ? classList.remove(className) : classList.add(className);
@@ -20,6 +31,11 @@
   function querySelector(root) {
       return function (selector) {
           return document.querySelector(selector);
+      }
+  }
+  function querySelectorAll(root) {
+      return function (selector) {
+          return document.querySelectorAll(selector);
       }
   }
 })(document);
